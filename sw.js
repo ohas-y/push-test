d14a63b78ffffb10d2d51cafe7d81efb76972cae
ohas-y <goodfellow_ohas@yahoo.co.jp>
2025-12-17 15:08:39 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
-self.addEventListener('push', function(event) {
+self.addEventListener('push', event => {
   const data = event.data.json();
+
   event.waitUntil(
     self.registration.showNotification(data.title, {
       body: data.body,
+      icon: 'icon.png',
+      data: { url: data.url }
     })
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
   );
 });
